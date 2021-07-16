d94dfbf969c1e7a8787de9368ed9622764416638
Elison Liu <elisonliu97@gmail.com>
2021-07-16 10:49:47 -0400

    route to create workouts

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -1,6 +1,18 @@
 const router = require("express").Router();
-const Workout = require("../../models/workout")
+const WorkoutDB = require("../../models/workout")
 
-
+// ROUTE TO CREATE NEW WORKOUT
+router.post('/', async (req, res) =>{
+    try{
+        // create entry in db
+        const workoutData = await WorkoutDB.create({
+            ... req.body
+        });
+        res.status(200).json(workoutData)
+    }
+    catch (err) {
+        res.status(400).json(err)
+    }
+})
 
 module.exports = router;
